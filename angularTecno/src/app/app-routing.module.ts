f22b1b478b82edcfcd9081c4f811dea5f738c1fb
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2023-04-21 11:40:20 +0000

    generic1.5

--- angularTecno/src/app/app-routing.module.ts
+++ angularTecno/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FruitsComponent } from './fruits/fruits.component';
 import { AnimalsComponent } from './animals/animals.component';
+import { GenericComponent } from './generic/generic.component';
 
 const routes: Routes = [
   
 { path: 'animals', component: AnimalsComponent},
-{ path: 'fruits', component: FruitsComponent}
+{ path: 'fruits', component: FruitsComponent},
+{path: '', redirectTo: '/animals', pathMatch: 'full'},
+{ path: 'generic/:id', component: GenericComponent }
 
 ];
 
